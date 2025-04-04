bed691215bd015d61b18732a236332de630b5b82
Robert S <berlamont@gmail.com>
2025-04-03 20:36:09 -0400

    feature/update-structure-45 - Refactor and remove legacy code
    - Removed obsolete files and classes:
      - `SplashActivity.cs`, `ContentPageTitleFontEffect.cs`, `DatePickerLineColorEffect.cs`, `EntryLineColorEffect.cs`, `MaxLineEffects.cs`, `PickerLineColorEffect.cs`, `UnderlineTextEffect.cs`, `CustomMapRenderer.cs`, `CustomMarkerOptions.cs`, `CustomProgressBarRenderer.cs`, `FloatingActionButtonViewRenderer.cs`, `ImageButtonRenderer.cs`, `NoBarsScrollViewerRenderer.cs`, `MainApplication.cs`, `AssemblyInfo.cs`, `AndroidManifest.xml`, `AboutResources.txt`.
    
    - Updated and modified key files:
      - `MainActivity.cs`, `Settings.cs`, `Resource.designer.cs`, `eoTouchDelivery.Android.csproj`, `packages.config`, `DialogService.cs`, `INavigationService.cs`, `iOSNavigationService.cs`, `NavigationService.cs`, `ViewModelLocator.cs`.
    
    - Introduced new UI components and styles:
      - Added `AboutAssets.txt`, `splash.svg`, `Colors.xaml`, `Styles.xaml`, and various new view models and validation rules.
    
    - Transitioned to .NET MAUI, enhancing the application structure and functionality.

---
+++ eoTouchDelivery/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace eoTouchDelivery
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
